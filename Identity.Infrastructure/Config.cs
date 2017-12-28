f4b2bd094db4223af6bfe4c6fb5c0dfe4c7af73b
Van Tian <vantian@live.com>
2017-12-28 20:56:22 +0800

    功能：添加API资源和客户端

---
+++ Identity.Infrastructure/Config.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using IdentityServer4.Models;
+
+namespace Identity.Infrastructure
+{
+    public static class Config
+    {
+
+        public static IEnumerable<ApiResource> GetAPIResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1","API 1")
+            };
+        }
+
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId="client",
+                    AllowedGrantTypes=GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes=
+                    {
+                        "api1"
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ Identity.Server/Startup.cs
@@ -0,0 +1,37 @@
+﻿using Identity.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Identity.Server
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryApiResources(Config.GetAPIResources())
+                .AddInMemoryClients(Config.GetClients());
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
