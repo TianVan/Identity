d290658071ab28ea1ec11ce1e1abe922908d0828
Van Tian <vantian@live.com>
2017-12-28 21:51:42 +0800

    功能：完成基于凭据的认证与授权

---
+++ IdentityServerSample.Client/Program.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using IdentityModel.Client;
+using System;
+
+namespace IdentityServerSample.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DiscoveryResponse response = DiscoveryClient.GetAsync("http://localhost:5000").Result;
+            var tokenClient = new TokenClient(response.TokenEndpoint, "client", "secret");
+            TokenResponse tokenResponse = tokenClient.RequestClientCredentialsAsync("api1").Result;
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+            Console.WriteLine(tokenResponse.Json);
+            Console.ReadKey();
+
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            HttpResponseMessage responseMessage = client.GetAsync("http://localhost:50001/api/identity").Result;
+
+            if (!responseMessage.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                Console.WriteLine(JArray.Parse(responseMessage.Content.ReadAsStringAsync().Result));
+            }
+        }
+    }
+}
