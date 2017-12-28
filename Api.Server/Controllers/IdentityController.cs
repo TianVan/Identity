3a7c115facfa351eb120168b2d9f84923099c90c
Van Tian <vantian@live.com>
2017-12-28 21:21:46 +0800

    功能：增加API服务

---
+++ Api.Server/Controllers/IdentityController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Server.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Identity")]
+    public class IdentityController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Json(User.Claims.Select(x => new { x.Type, x.Value }));
+        }
+    }
+}
